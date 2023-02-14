34f24e3b7fd94afe32de6e5f168eeb813153796f
Salman Patwary <114179944+Salman1881420@users.noreply.github.com>
2023-02-14 20:17:49 +0600

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -22,12 +22,8 @@ async def _start(c: UtubeBot, m: Message):
                   [
                       InlineKeyboardButton("How To Use Me🙄", callback_data="/help")
                   ],
-                  [
-                      InlineKeyboardButton("Project Channel!", url="https://t.me/hxbots"),
-                      InlineKeyboardButton("Support Group", url="https://t.me/HxSupport")
-                  ],
                   [  
-                      InlineKeyboardButton("Buy Me A Tea", url="https://upayme.vercel.app/kkirodewal@ybl")
+                      InlineKeyboardButton("Admin", url="https://t.me/Salman1881")
                   ]]
         ),
     )
